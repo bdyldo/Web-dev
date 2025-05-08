5b2f1065e075a6869377b8b331ab232e78369705
bdyldo <dyl123455@outlook.com>
2025-05-07 23:29:17 -0400

    Added delete button on checkout page

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,4 +1,10 @@
-export const cart = []; 
+export const cart = [{
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2
+},{
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1
+}]; 
 
 // Helper function to add element into cart: If button clicked: add 1 quantity to the cart if already in cart, else create new element in cart
 export function addToCart(productId){
@@ -11,6 +17,13 @@ export function addToCart(productId){
             productId,
             quantity: 1
         });
+    }
+}
+
+export function removeFromCart(productId){
+    const idx = cart.findIndex(i => i.productId === productId)
+    if (idx !== -1) {
+        cart.splice(idx, 1); 
     }
 }
 
